e25355260dceb04792097918661b03060ed01f64
Nemanja <gaonlb9333333@gmail.com>
2026-06-02 23:21:14 +0200

    fix: bump service worker cache to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'swr-v1';
+const CACHE = 'swr-v3';
 const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
